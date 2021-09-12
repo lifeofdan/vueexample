6621933f27d41c55dbb03fbcfa2affaedbca9d24
Daniel Newman <contact@lifeofdan.me>
2021-09-12 23:16:29 +1000

    init

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-start">
+    <div class="mb-3 text-start">
+      <label for="exampleFormControlInput1" class="form-label">Name</label>
+      <input type="text" class="form-control" placeholder="John Smith">
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlInput2" class="form-label">Email address</label>
+      <input type="email" class="form-control" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlInput3" class="form-label">Message</label>
+      <textarea class="form-control" placeholder="Type your message here."></textarea>
+    </div>
+    <button class="btn btn-primary">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ pageName }}</h1>
+    <div class="container">
+      <ContactForm />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import ContactForm from '../components/ContactForm.vue'
+export default {
+  name: 'ContactPage',
+  components: { ContactForm },
+  setup () {
+    const pageName = 'Contact Page'
+
+    return { pageName }
+  }
+}
+</script>
+
+<style>
+ .red {
+   color: red;
+ }
+</style>
